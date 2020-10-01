1ffcea67588ddf7a0d95d873977b19c8ec6d181e
Hannah <70531893+talosguideu@users.noreply.github.com>
2020-10-01 12:47:29 -0600

    Add files via upload

---
+++ Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+//assign variables//
+var Creatures = "Fairy Tale Things", Ogre = "Shrek", Sidekick = "Donkey", Cookie = "Gingy";
+//assign colors to variables//
+var Creatures = Creatures.fontcolor("yellow");
+var Ogre = Ogre.fontcolor("green");
+var Sidekick = Sidekick.fontcolor("brown");
+var Cookie = Cookie.fontcolor("blue");
+//concatenate a string//
+var legendaryQuote = "Get out of"
++" my swamp!!!";
+//display quote//
+document.write(legendaryQuote);
+//display "shrek" on screen//
+document.write(Ogre);
+//display "25" on screen//
+window.alert(18+7);
