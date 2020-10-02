eae78add1ee6c135981c5822d83c16ff86808aea
Hannah <70531893+talosguideu@users.noreply.github.com>
2020-10-02 10:58:08 -0600

    Add files via upload

---
+++ Project2/JS/main.js
@@ -0,0 +1,28 @@
+//assign variables//
+var Creatures = "Fairy Tale Things", Ogre = "Shrek", Sidekick = "Donkey", Cookie = "Gingy";
+//assign colors to variables//
+var Creatures = Creatures.fontcolor("yellow");
+var Ogre = Ogre.fontcolor("green");
+var Sidekick = Sidekick.fontcolor("brown");
+var Cookie = Cookie.fontcolor("blue");
+//concatenate a string//
+var legendaryQuote = "Get out of"
++" my swamp!!!";
+//display quote//
+document.write(legendaryQuote);
+//display "shrek" on screen//
+document.write(Ogre);
+//display "25" on screen//
+window.alert(18+7);
+
+//Create a function that assigns a sentence to a variable and calls that sentence when something is clicked on//
+function myFunction(){
+    var sentence="Not all who wander";
+    sentence+=" are lost...";
+    document.getElementById("profound").innerHTML=sentence;
+}
+
+function addition_Function(){
+    var addition=2+2;
+    document.getElementById("Math").innerHTML="2+2="+ addition;
+}
